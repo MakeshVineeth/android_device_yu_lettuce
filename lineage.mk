613cc844f876c70ccd7246b298c3e26e1a67fea1
Makesh Vineeth <makeshvineeth1997@gmail.com>
2020-04-22 15:46:35 +0530

    rm full_lettuce and imports in lineage.mk itself

--- lineage.mk
+++ lineage.mk
@@ -13,26 +13,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, device/yu/lettuce/full_lettuce.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l.mk)
+
+# Inherit from lettuce device
+$(call inherit-product, device/yu/lettuce/device.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# lettuce was launched with Android LP
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l.mk)
-
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
 
+# Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_lettuce
 BOARD_VENDOR := yu
 PRODUCT_DEVICE := lettuce
-
-# Vendor security patch level
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.lineage.build.vendor_security_patch=2016-05-01
-
-TARGET_BOOT_ANIMATION_RES := 720
+PRODUCT_BRAND := YU
+PRODUCT_MODEL := YU5010
+PRODUCT_MANUFACTURER := YU
 
 PRODUCT_GMS_CLIENTID_BASE := android-micromax
 
@@ -41,6 +42,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=YUPHORIA \
     TARGET_DEVICE=YUPHORIA
 
- BUILD_FINGERPRINT=YU/YUPHORIA/YUPHORIA:$(PLATFORM_VERSION)/$(BUILD_ID)/MMXMR1:$(TARGET_BUILD_VARIANT)/release-keys
-
-TARGET_OTA_ASSERT_DEVICE := YUPHORIA,lettuce,YU5010,YU5010A
+BUILD_FINGERPRINT=YU/YUPHORIA/YUPHORIA:$(PLATFORM_VERSION)/$(BUILD_ID)/MMXMR1:$(TARGET_BUILD_VARIANT)/release-keys
