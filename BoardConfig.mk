8451f6046f1ceb036083eda2f355a7776eb5b2ca
Makesh Vineeth <makeshvineeth1997@gmail.com>
2020-05-12 12:06:11 +0530

    TARGET_KERNEL_CONFIG: yuphoria_lettuce_defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -153,7 +153,7 @@ BOARD_RAMDISK_OFFSET := 0x02000000
 LZMA_RAMDISK_TARGETS := recovery
 
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/aarch64/aarch64-opt-linux-android/bin
-TARGET_KERNEL_CONFIG := reborn_lettuce_defconfig
+TARGET_KERNEL_CONFIG := yuphoria_lettuce_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-opt-linux-android-
 TARGET_KERNEL_SOURCE := kernel/cyanogen/msm8916
 
