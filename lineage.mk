81e4c386e4438560570eca115e78a4f88b309876
MakeshVineeth <MakeshVineeth>
2020-08-04 19:26:50 +0530

    revert battery saving

--- lineage.mk
+++ lineage.mk
@@ -79,3 +79,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.type=user \
     ro.build.description=bonito-user 10 QQ3A.200705.002 6506677 release-keys \
     ro.build.version.security_patch=2020-07-05
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+	ro.build.fingerprint=$(BUILD_FINGERPRINT) \
+    ro.build.version.security_patch=2020-07-05
